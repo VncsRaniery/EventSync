5f4a7839b89b76f0d64c3b92e7aa05a1d8298067
Vinícius Raniery <152623369+VncsRaniery@users.noreply.github.com>
2024-08-10 16:46:46 -0300

    commit

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const headerLinks = [
+  {
+    label: 'Início',
+    route: '/',
+  },
+  {
+    label: 'Criar um evento',
+    route: '/events/create',
+  },
+  {
+    label: 'Meu Perfil',
+    route: '/profile',
+  },
+]
+
+export const eventDefaultValues = {
+  title: '',
+  description: '',
+  location: '',
+  imageUrl: '',
+  startDateTime: new Date(),
+  endDateTime: new Date(),
+  categoryId: '',
+  price: '',
+  isFree: false,
+  url: '',
+}
